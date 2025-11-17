24f1f2640f85be081ad8c66ffc8838f8ab741767
JoonKyoLee <joonkyolee@gmail.com>
2025-11-18 01:22:24 +0900

    feat: 전역 예외 처리용 GlobalExceptionHandler 구현

---
+++ src/main/java/com/almang/inventory/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.almang.inventory.global.exception;
+
+import com.almang.inventory.global.api.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception exception) {
+        log.error("[Exception]", exception);
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ApiResponse.fail(errorCode));
+    }
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ApiResponse<Void>> handleBaseException(BaseException baseException) {
+        log.error("[BaseException]", baseException);
+        ErrorCode errorCode = baseException.getErrorCode();
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ApiResponse.fail(errorCode));
+    }
+}

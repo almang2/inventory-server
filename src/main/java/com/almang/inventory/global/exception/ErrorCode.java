8e64e08badcee1a9464e7bc41d06806dad3fb16d
JoonKyoLee <joonkyolee@gmail.com>
2025-11-18 01:13:44 +0900

    feat: 전역 예외 처리를 위한 ErrorCode 추가

---
+++ src/main/java/com/almang/inventory/global/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.almang.inventory.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}

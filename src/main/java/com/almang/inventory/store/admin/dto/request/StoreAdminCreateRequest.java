e60b941f92eaebc4c44b20c47690878fa65ed6ab
JoonKyoLee <joonkyolee@gmail.com>
2025-11-18 09:29:50 +0900

    refactor: 필요 없는 import문 삭제

--- src/main/java/com/almang/inventory/store/admin/dto/request/StoreAdminCreateRequest.java
+++ src/main/java/com/almang/inventory/store/admin/dto/request/StoreAdminCreateRequest.java
@@ -1,7 +1,5 @@
 package com.almang.inventory.store.admin.dto.request;
 
-import com.almang.inventory.user.domain.UserRole;
-
 public record StoreAdminCreateRequest(
         String username,
         String name,

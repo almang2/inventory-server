ad89c3b851aa90f5ac6cfb5344b5b581af6d6f0d
JoonKyoLee <joonkyolee@gmail.com>
2025-11-18 09:03:04 +0900

    refactor: Store 도메인 생성으로 User 도메인 내의 store 연관관계 설정

--- src/main/java/com/almang/inventory/user/domain/User.java
+++ src/main/java/com/almang/inventory/user/domain/User.java
@@ -1,13 +1,17 @@
 package com.almang.inventory.user.domain;
 
 import com.almang.inventory.global.entity.BaseTimeEntity;
+import com.almang.inventory.store.domain.Store;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -29,6 +33,10 @@ public class User extends BaseTimeEntity {
     @Column(name = "user_id")
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
     @Column(name = "username", length = 20, nullable = false, unique = true)
     private String username;
 
@@ -41,10 +49,6 @@ public class User extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false)
     private UserRole role;
-
-    // TODO: Store 엔티티 생성 전까지 임시로 Long으로 설정
-    @Column(name = "store_id", nullable = false)
-    private Long storeId;
 
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;

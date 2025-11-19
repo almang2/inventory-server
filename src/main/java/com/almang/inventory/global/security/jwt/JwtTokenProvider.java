84ea5c6c92b520fead38e87148d48883ab2728d0
JoonKyoLee <joonkyolee@gmail.com>
2025-11-19 20:09:54 +0900

    feat: 토큰의 유효성을 검증하고 토큰에서 유저의 정보를 추출하는 메서드 추가

--- src/main/java/com/almang/inventory/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/almang/inventory/global/security/jwt/JwtTokenProvider.java
@@ -1,5 +1,8 @@
 package com.almang.inventory.global.security.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -34,5 +37,41 @@ public class JwtTokenProvider {
                 .setExpiration(expiryDate)
                 .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public TokenStatus validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return TokenStatus.VALID;
+        } catch (ExpiredJwtException e) {
+            return TokenStatus.EXPIRED;
+        } catch (JwtException e) {
+            return TokenStatus.INVALID;
+        }
+    }
+
+    public Long getUserIdFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public long getRemainingMillis(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        long exp = claims.getExpiration().getTime();
+        long now = System.currentTimeMillis();
+        return Math.max(0L, exp - now);
     }
 }

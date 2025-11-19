5eb0b5119f9fde046186cc6bd36aca6f85ea3e81
JoonKyoLee <joonkyolee@gmail.com>
2025-11-20 01:49:38 +0900

    feat: 사용자 프로필 정보 조회 서비스 로직 추가

---
+++ src/main/java/com/almang/inventory/user/service/UserService.java
@@ -0,0 +1,38 @@
+package com.almang.inventory.user.service;
+
+import com.almang.inventory.global.exception.BaseException;
+import com.almang.inventory.global.exception.ErrorCode;
+import com.almang.inventory.store.domain.Store;
+import com.almang.inventory.store.repository.StoreRepository;
+import com.almang.inventory.user.domain.User;
+import com.almang.inventory.user.dto.response.UserProfileResponse;
+import com.almang.inventory.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public UserProfileResponse getUserProfile(Long userId) {
+        User user = findUserById(userId);
+
+        log.info("[UserService] 사용자 정보 조회 요청 - userId: {}", user.getId());
+        Store store = user.getStore();
+
+        log.info("[UserService] 사용자 정보 조회 성공 - userId: {}", user.getId());
+        return UserProfileResponse.from(user, store);
+    }
+
+
+    private User findUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new BaseException(ErrorCode.USER_NOT_FOUND));
+    }
+}

1388c57a4bfbc78646b4d2d28cd06740290c56d5
JoonKyoLee <joonkyolee@gmail.com>
2025-11-20 03:22:13 +0900

    feat: 회원 탈퇴 서비스 로직 추가

--- src/main/java/com/almang/inventory/user/service/UserService.java
+++ src/main/java/com/almang/inventory/user/service/UserService.java
@@ -3,11 +3,15 @@ package com.almang.inventory.user.service;
 import com.almang.inventory.global.exception.BaseException;
 import com.almang.inventory.global.exception.ErrorCode;
 import com.almang.inventory.store.domain.Store;
+import com.almang.inventory.user.auth.service.AuthService;
 import com.almang.inventory.user.domain.User;
 import com.almang.inventory.user.dto.request.UpdateUserProfileRequest;
+import com.almang.inventory.user.dto.response.DeleteUserResponse;
 import com.almang.inventory.user.dto.response.UpdateUserProfileResponse;
 import com.almang.inventory.user.dto.response.UserProfileResponse;
 import com.almang.inventory.user.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final AuthService authService;
 
     @Transactional(readOnly = true)
     public UserProfileResponse getUserProfile(Long userId) {
@@ -40,6 +45,18 @@ public class UserService {
 
         log.info("[UserService] 사용자 정보 수정 성공 - userId: {}", user.getId());
         return new UpdateUserProfileResponse(true);
+    }
+
+    @Transactional
+    public DeleteUserResponse deleteUser(Long userId, HttpServletRequest request, HttpServletResponse response) {
+        User user = findUserById(userId);
+
+        log.info("[UserService] 회원 탈퇴 요청 - userId: {}", user.getId());
+        user.delete();
+        authService.logout(userId, request, response);
+
+        log.info("[UserService] 회원 탈퇴 성공 - userId: {}", user.getId());
+        return new DeleteUserResponse(true);
     }
 
     private User findUserById(Long id) {

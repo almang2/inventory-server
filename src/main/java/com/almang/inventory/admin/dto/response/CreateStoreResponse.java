9dd1dcac583cf6005aa542ede20f2177765adff6
JoonKyoLee <joonkyolee@gmail.com>
2025-11-20 10:59:59 +0900

    feat: 상점 등록 서비스 로직 구현

--- src/main/java/com/almang/inventory/admin/dto/response/CreateStoreResponse.java
+++ src/main/java/com/almang/inventory/admin/dto/response/CreateStoreResponse.java
@@ -1,9 +1,20 @@
 package com.almang.inventory.admin.dto.response;
 
+import com.almang.inventory.store.domain.Store;
 import java.math.BigDecimal;
 
 public record CreateStoreResponse(
+        Long storeId,
         String name,
         boolean isActivate,
         BigDecimal defaultCountCheckThreshold
-) {}
+) {
+    public static CreateStoreResponse from(Store store) {
+        return new CreateStoreResponse(
+                store.getId(),
+                store.getName(),
+                store.isActivate(),
+                store.getDefaultCountCheckThreshold()
+        );
+    }
+}

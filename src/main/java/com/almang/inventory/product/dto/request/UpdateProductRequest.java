65da47f47096e4b7b3b0a4ac19153d721a1ac2ae
JoonKyoLee <joonkyolee@gmail.com>
2025-11-20 17:25:55 +0900

    fix: 원하는 항목만 업데이트 할 수 있도록 Primitive type으로 변경

--- src/main/java/com/almang/inventory/product/dto/request/UpdateProductRequest.java
+++ src/main/java/com/almang/inventory/product/dto/request/UpdateProductRequest.java
@@ -1,10 +1,7 @@
 package com.almang.inventory.product.dto.request;
 
 import com.almang.inventory.product.domain.ProductUnit;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 
 public record UpdateProductRequest(
@@ -13,10 +10,10 @@ public record UpdateProductRequest(
         String code,
         ProductUnit unit,
         BigDecimal boxWeightG,
-        @Positive int unitPerBox,
+        Integer unitPerBox,
         BigDecimal unitWeightG,
-        boolean isActivate,
-        @Min(0) int costPrice,
-        @Min(0) int retailPrice,
-        @Min(0) int wholesalePrice
+        Boolean isActivate,
+        Integer costPrice,
+        Integer retailPrice,
+        Integer wholesalePrice
 ) {}

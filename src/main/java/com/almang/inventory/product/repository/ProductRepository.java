5d6d6fd9c9b35d840e55fffab3895dc421156f0e
JoonKyoLee <joonkyolee@gmail.com>
2025-11-21 01:06:51 +0900

    feat: 품목 조회 레포지토리 메서드 추가

--- src/main/java/com/almang/inventory/product/repository/ProductRepository.java
+++ src/main/java/com/almang/inventory/product/repository/ProductRepository.java
@@ -1,7 +1,23 @@
 package com.almang.inventory.product.repository;
 
 import com.almang.inventory.product.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findAllByStoreId(Long storeId, Pageable pageable);
+
+    Page<Product> findAllByStoreIdAndActivateIsTrue(
+            Long storeId, Pageable pageable
+    );
+
+    Page<Product> findAllByStoreIdAndNameContainingIgnoreCase(
+            Long storeId, String name, Pageable pageable
+    );
+
+    Page<Product> findAllByStoreIdAndActivateIsTrueAndNameContainingIgnoreCase(
+            Long storeId, String name, Pageable pageable
+    );
 }

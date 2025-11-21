6acd255d6cecc48ac238502c74048835bf85257e
JoonKyoLee <joonkyolee@gmail.com>
2025-11-21 12:34:41 +0900

    feat: 리포지토리 내의 조회 메서드 추가

--- src/main/java/com/almang/inventory/vendor/repository/VendorRepository.java
+++ src/main/java/com/almang/inventory/vendor/repository/VendorRepository.java
@@ -1,7 +1,23 @@
 package com.almang.inventory.vendor.repository;
 
 import com.almang.inventory.vendor.domain.Vendor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VendorRepository extends JpaRepository<Vendor, Long> {
+
+    Page<Vendor> findAllByStoreId(Long storeId, Pageable pageable);
+
+    Page<Vendor> findAllByStoreIdAndActivatedTrue(
+            Long storeId, Pageable pageable
+    );
+
+    Page<Vendor> findAllByStoreIdAndNameContainingIgnoreCase(
+            Long storeId, String name, Pageable pageable
+    );
+
+    Page<Vendor> findAllByStoreIdAndActivatedTrueAndNameContainingIgnoreCase(
+            Long storeId, String name, Pageable pageable
+    );
 }

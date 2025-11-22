3ce33c700e6309a7cec3bb5dc11d0366fc77a08a
JoonKyoLee <joonkyolee@gmail.com>
2025-11-22 16:46:21 +0900

    refactor: 발주 템플릿 수정 요청 시에 발주처 아이디를 포함해야 했던 부분 제외

--- src/main/java/com/almang/inventory/order/template/dto/request/UpdateOrderTemplateRequest.java
+++ src/main/java/com/almang/inventory/order/template/dto/request/UpdateOrderTemplateRequest.java
@@ -1,9 +1,6 @@
 package com.almang.inventory.order.template.dto.request;
 
-import jakarta.validation.constraints.NotNull;
-
 public record UpdateOrderTemplateRequest(
-        @NotNull Long vendorId,
         String title,
         String body,
         Boolean activated

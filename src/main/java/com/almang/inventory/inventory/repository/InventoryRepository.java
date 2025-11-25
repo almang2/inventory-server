fc76191a718b715ead1e6d0e794ff57e94e425c8
JoonKyoLee <joonkyolee@gmail.com>
2025-11-25 16:10:12 +0900

    feat: InventoryRepository 내에 제품 아이디로 재고를 조회할 수 있도록 메서드 추가

--- src/main/java/com/almang/inventory/inventory/repository/InventoryRepository.java
+++ src/main/java/com/almang/inventory/inventory/repository/InventoryRepository.java
@@ -1,7 +1,10 @@
 package com.almang.inventory.inventory.repository;
 
 import com.almang.inventory.inventory.domain.Inventory;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    Optional<Inventory> findByProduct_Id(Long productId);
 }

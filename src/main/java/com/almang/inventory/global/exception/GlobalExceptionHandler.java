a7983e3d650770d438881740cb6b43c54bb6955c
JoonKyoLee <joonkyolee@gmail.com>
2025-11-28 22:59:00 +0900

    feat: GlobalExceptionHandler에 Discord 에러 알림 연동

--- src/main/java/com/almang/inventory/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/almang/inventory/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.almang.inventory.global.exception;
 
 import com.almang.inventory.global.api.ApiResponse;
+import com.almang.inventory.global.monitoring.DiscordErrorNotifier;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,11 +12,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
+    private final DiscordErrorNotifier discordErrorNotifier;
+
     @ExceptionHandler(BaseException.class)
-    public ResponseEntity<ApiResponse<Void>> handleBaseException(BaseException baseException) {
+    public ResponseEntity<ApiResponse<Void>> handleBaseException(
+            BaseException baseException, HttpServletRequest request
+    ) {
         log.error("[BaseException]", baseException);
+        discordErrorNotifier.notifyException(baseException, request);
         ErrorCode errorCode = baseException.getErrorCode();
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
@@ -21,17 +30,22 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<Void>> handleValidationException(MethodArgumentNotValidException e) {
+    public ResponseEntity<ApiResponse<Void>> handleValidationException(
+            MethodArgumentNotValidException e, HttpServletRequest request
+    ) {
         log.warn("[ValidationException] {}", e.getMessage());
-
+        discordErrorNotifier.notifyException(e, request);
         return ResponseEntity
                 .status(ErrorCode.INVALID_INPUT_VALUE.getHttpStatus())
                 .body(ApiResponse.fail(ErrorCode.INVALID_INPUT_VALUE));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<Void>> handleException(Exception exception) {
+    public ResponseEntity<ApiResponse<Void>> handleException(
+            Exception exception, HttpServletRequest request
+    ) {
         log.error("[Exception]", exception);
+        discordErrorNotifier.notifyException(exception, request);
         ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
